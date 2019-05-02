c567f0fb039ef5d663d55d9abd8aa926887ebbb9
m4rc0s <marquinhoaraujo@gmail.com>
2019-05-02 17:49:42 -0300

    testing cors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    const instance = axios.create({
+      baseURL: 'https://ajks56yzga.execute-api.sa-east-1.amazonaws.com/ApiSouPi/mgm/v1/',
+      timeout: 1000,
+      headers: { 
+        'x-api-key': 'aA9rh6WLH76fPdO0FJcwz2qHUQ7HzCY85NgtnyOJ',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+
+    instance.get('members/559802900/gains').then((data) => {
+      console.log('data', data);
+    })
+    
+  }
+
   render() {
     return (
       <div className="App">
